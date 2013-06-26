bbad8a421a2677672b9e7d541faaa2f1526462ed
Tom Najdek <tom@doppnet.com>
2013-06-26 19:30:11 +0100

    Removed unnecessary comment.

--- app/js/main.js
+++ app/js/main.js
@@ -20,12 +20,4 @@ require( [
 ], function(angular, app, routes) {
 	'use strict';
 		angular.bootstrap(document, [app['name']]);
-		// Because of RequireJS we need to bootstrap the app app manually
-		// and Angular Scenario runner won't be able to communicate with our app
-		// unless we explicitly mark the container as app holder
-		
-		// You must have ng-app specified in your in your .html file (eg. <html ng-app > )
-		// In order for Karma to run the tests
-		
-		// More info: https://groups.google.com/forum/#!msg/angular/yslVnZh9Yjk/MLi3VGXZLeMJ
 });
