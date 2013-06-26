4ea2866c63ddcbfc35930df1fdd86568a2f8cc1d
jadekler <jadekler@gmail.com>
2013-06-26 13:21:09 -0400

    Fixed routing problems

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ require.config({
 		angular: 'lib/angular/angular',
 		text: 'lib/require/text'
 	},
-	baseUrl: '/app/js',
+	baseUrl: 'app/js',
 	shim: {
 		'angular' : {'exports' : 'angular'},
 		'angularMocks': {deps:['angular'], 'exports':'angular.mock'}
--- app/js/routes.js
+++ app/js/routes.js
@@ -3,11 +3,11 @@ define(['angular', 'app'], function(angular, app) {
 
 	return app.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when('/view1', {
-			templateUrl: '/app/partials/partial1.html',
+			templateUrl: 'app/partials/partial1.html',
 			controller: 'MyCtrl1'
 		});
 		$routeProvider.when('/view2', {
-			templateUrl: '/app/partials/partial2.html',
+			templateUrl: 'app/partials/partial2.html',
 			controller: 'MyCtrl2'
 		});
 		$routeProvider.otherwise({redirectTo: '/view1'});
