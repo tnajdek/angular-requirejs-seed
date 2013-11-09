0a298f9c2ccdc53cc1583e4e04a23b4663a56340
Dane Petersen <thegreatsunra@gmail.com>
2013-11-09 07:04:23 -0800

    Add comments from angular-seed.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,6 +1,8 @@
 define(['angular', 'services'], function (angular) {
 	'use strict';
 
+	/* Controllers */
+	
 	return angular.module('myApp.controllers', ['myApp.services'])
 		// Sample controller where service is being used
 		.controller('MyCtrl1', ['$scope', 'version', function ($scope, version) {
