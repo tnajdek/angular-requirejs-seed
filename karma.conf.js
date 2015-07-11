4de532ea7900c2e381a6572d2661392da5a47a9a
Joel Wang <gastlygem@gmail.com>
2015-07-11 21:20:26 +0800

    surprise surprise. Now it works!

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test-main.js',
+      'test/test-main.js',
       {pattern: 'app/**/*.js', included: false},
       {pattern: 'test/**/*.js', included: false},
     ],
